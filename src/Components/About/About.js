312c0f9c280bfc4f4d97047dee9590d4b78f6f06
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-10-04 20:15:15 +0600

    add something new

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,30 @@
 import React from "react";
-
+import "./About.css";
+import aboutImg from "../../images/about-me_01.jpg";
 const About = () => {
   return (
     <div className="text-center pt-5 mt-5">
-      <h2>This is About Page</h2>
+      <div className="about-sec">
+        <h2 className="fw-bolder ">About Us</h2>
+      </div>
+      <div className="elemenetor-container">
+        <div className="col-6">
+          <img src={aboutImg} alt="" />
+        </div>
+        <div className="col-6">
+          <span className="about-us-title fw-bolder">About us</span>
+          <h1 className="fw-bolder pt-4">
+            We Help People Who has Lost in the Life Cycle
+          </h1>
+          <p className="text-muted">
+            When you come to coaching or counseling, we work with you, your
+            situation / issue and together we find the best solution for you.
+            All the tools we will use, you will be able to apply later in life,
+            without necessarily turning to the coach every time.
+          </p>
+          <button className="about-contact-btn fw-bolder">Contact Us</button>
+        </div>
+      </div>
     </div>
   );
 };
