3519738fb144845e531b1831999c0cb54857f6a2
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-10-04 18:58:07 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import NotFound from "./Components/NotFound/NotFound";
@@ -11,20 +12,26 @@ function App() {
   return (
     <div className="App">
       <Router>
+        {/* header content  */}
         <Header></Header>
         <Switch>
+          {/* if path route is "/"  */}
           <Route exact path="/">
             <Home></Home>
           </Route>
+          {/* home route  */}
           <Route exact path="/home">
             <Home></Home>
           </Route>
+          {/* services route  */}
           <Route exact path="/services">
             <Services></Services>
           </Route>
+          {/* about route  */}
           <Route exact path="/about">
             <About></About>
           </Route>
+          {/* contact route  */}
           <Route exact path="/contact">
             <Contact></Contact>
           </Route>
@@ -33,6 +40,8 @@ function App() {
             <NotFound></NotFound>
           </Route>
         </Switch>
+        {/* footer content  */}
+        <Footer></Footer>
       </Router>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,40 @@
-import React from 'react';
-
+import React from "react";
+import "./Footer.css";
+import logo from "../../images/logo-B.png";
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="footer">
+      <hr className="footer-seperator" />
+      <div className="footer-logo">
+        <img src={logo} alt="" />
+      </div>
+      <section className="footer-info">
+        <section className="footer-info-left">
+          <section className="footer-info__name">UniCoach</section>
+          <section className="footer-info__returns">
+            Returns Policy
+            <br />
+            Delivery
+          </section>
+        </section>
+        <section className="footer-info-center">
+          <section className="footer-info__email">
+            unicoach.info@gmail.com
+          </section>
+          <section className="footer-info__terms">Terms and Conditions</section>
+        </section>
+        <section className="footer-info-right">
+          <section className="footer-info__number">+8801307767187</section>
+          <section className="footer-info__contact">
+            Success Story
+            <br />
+            Contact Us
+          </section>
+        </section>
+      </section>
+      <hr className="footer-seperator" />
+    </section>
+  );
 };
 
 export default Footer;
