4132a1fea8b5f36a15dcd83765d4c094dc1ff25a
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-10-04 12:59:47 +0600

    show error

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import "./NotFound.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 const NotFound = () => {
   return (
-    <div className="text-center mt-5">
-      <h1>404</h1>
+    <div className="text-center mt-5 not-found">
+      <FontAwesomeIcon className="not-found-icon" icon={faExclamationTriangle} />
+      <h1 className="pt-2">404</h1>
       <br />
-      <p>Page Not Found</p>
+      <p className="ps-2">Page Not Found</p>
     </div>
   );
 };
