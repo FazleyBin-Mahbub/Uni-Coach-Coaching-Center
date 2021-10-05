7bf64cbb67cddccb9539b8a7bd448d97e493c1e7
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-10-05 15:52:04 +0600

    footer code update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,35 +4,35 @@ import logo from "../../images/logo-B.png";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <section className="footer">
+    <div className="footer">
       <hr className="footer-seperator" />
       <div className="footer-logo">
         <Link to="/home">
           <img src={logo} alt="" />
         </Link>
       </div>
-      <section className="footer-info">
-        <section className="footer-info-left">
+      <div className="footer-info">
+        <div className="footer-info-left">
           <h1 className="footer-info__name">UniCoach</h1>
           <p className="text-start">Your Coaching Partner</p>
           <h6 className="footer-info__returns">Returns Policy</h6>
-        </section>
-        <section className="footer-info-center">
+        </div>
+        <div className="footer-info-center">
           <h5 className="footer-info__email">unicoach.info@gmail.com</h5>
           <h6 className="footer-info__terms">Terms and Conditions</h6>
-        </section>
-        <section className="footer-info-right">
+        </div>
+        <div className="footer-info-right">
           <h5 className="footer-info__number">+8801307767187</h5>
           <h6 className="footer-info__contact">
             Success Story
             <br />
             Contact Us
           </h6>
-        </section>
-      </section>
+        </div>
+      </div>
       <hr className="footer-seperator" />
       <p> Copyright &copy; Uni-Coach 2021 @Fazley Bin Mahbub</p>
-    </section>
+    </div>
   );
 };
 
